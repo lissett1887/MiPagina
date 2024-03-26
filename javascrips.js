009ab28aa54a0e7026ae92c49a6df67c3e23e015
lissett1887 <162449609+lissett1887@users.noreply.github.com>
2024-03-26 11:11:57 -0300

    Add files via upload

---
+++ javascrips.js
@@ -0,0 +1,29 @@
+// Declarando las variables string
+
+let nombre = "esteban"
+let apellido = "cisterna"
+
+//declarando los inregers
+
+let edad = 27
+let direccion = 120
+var estatura = 1.74
+var año = 2024
+
+//declarando los booleanos
+let regristrado = false
+let humano = true
+let humanoide = false
+let logeado = true
+
+//declarando los arreglos
+let arreglo = ["esteban","3°b", 27, true]
+let arregloVacio = []
+let arregloConValores = [nonbre. apellido]
+let arreglito = [0,22,[1,2]]
+
+//declarando objetos
+let persona = {curso:"3°b", generacion: 2024}
+let alumno = {nombre: "esteban", apellido: "cisterna"}
+let objetoVacio = {}
+let alumne = {nombre:nombre, apellido:apellido, edad:edad}
